e23f5ed849f2bee8e96ac317728a462c528595a6
Piotrek Pszczoła <piotreke12@gmail.com>
2015-12-01 22:25:16 +0100

    add association between link and user

---
+++ db/migrate/20151201212340_add_user_id_to_links.rb
@@ -0,0 +1,4 @@
+class AddUserIdToLinks < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201203046) do
+ActiveRecord::Schema.define(version: 20151201212340) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
